a1edc2da48539a43e3429f3f1dd14a1d33211a39
terryhycheng <terryhycheng@gmail.com>
2023-05-12 23:52:36 +0100

    Initial setup from Pobl

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      {/* Replace this with your code ...*/}
+      <img src='/pobl-logo.png' className="App-logo" alt="logo" />
+      <h2>New Starter Requests</h2>
+      <p>Here is the blank project for you to start from for this technical test. <br/> See <a href="https://github.com/PoblGroup-Dev/Junior-Web-Dev-Practical" target="_blank" rel="noreferrer">readme</a> for information. Good Luck!</p>
+      {/* ... */}
+      {/* New User Request Form */}
+      {/* New User Request List  */}
+    </div>
+  );
+}
+
+export default App;
