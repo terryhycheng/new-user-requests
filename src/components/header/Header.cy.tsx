f9190c02458493a053b76be7789f3b3a284643b8
terryhycheng <terryhycheng@gmail.com>
2023-05-14 21:17:44 +0100

    Created tests for Header

---
+++ src/components/header/Header.cy.tsx
@@ -0,0 +1,27 @@
+import Header from "./Header";
+
+describe("<Header />", () => {
+  beforeEach(() => cy.mount(<Header />));
+
+  it("should render a GitHub logo", () => {
+    cy.get('[data-cy="github-logo"]')
+      .should("exist")
+      .and("have.attr", "src", "/github.svg");
+  });
+
+  it("should have a link to the GitHub repo", () => {
+    cy.get('[data-cy="github-link"]')
+      .should("exist")
+      .and(
+        "have.attr",
+        "href",
+        "https://github.com/terryhycheng/new-user-requests"
+      );
+  });
+
+  it("should render a Pobl logo", () => {
+    cy.get('[data-cy="pobl-logo"]')
+      .should("exist")
+      .and("have.attr", "src", "/pobl-logo.png");
+  });
+});
