2bf8b1732e88a3f685402057b8281b6f7a9ff597
terryhycheng <terryhycheng@gmail.com>
2023-05-14 22:56:04 +0100

    Defined data types

---
+++ types/data.d.ts
@@ -0,0 +1,9 @@
+interface Data {
+  id: number;
+  firstName: string;
+  lastName: string;
+  jobTitle: string;
+  lineManager: string;
+  startDate: string;
+  businessArea: "IT" | "Finance" | "HR" | "Housing" | "Care";
+}
